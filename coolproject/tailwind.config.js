bc5716ffda0106b49e54f08709e068567f8fc56a
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2024-03-05 13:17:29 -0500

    u da king.. burger king..

---
+++ coolproject/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+/* please note vue-tailwind does not seem to work with current vue */
+export default {
+  content: [],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
