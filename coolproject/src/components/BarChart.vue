726fe5df544aa7dfabeaf57f95ba134c227b13fa
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2024-03-13 13:20:17 -0400

    iteration needs redoing

--- coolproject/src/components/BarChart.vue
+++ coolproject/src/components/BarChart.vue
@@ -17,24 +17,18 @@ import {
 } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-const boroughData = [
-  { borough: 'B', count: 0 },
-  { borough: 'S', count: 0 },
-  { borough: 'K', count: 0 },
-  { borough: 'M', count: 0 },
-  { borough: 'Q', count: 0 }
-]
+
 export default {
   name: 'BarChart',
   components: { Bar },
   loaded: false,
   data: () => ({
     chartData: {
-      labels: boroughData.map((row) => row.borough),
+      labels: null, // boroughData.map((row) => row.borough),
       datasets: [
         {
           label: 'test',
-          data: boroughData.map((row) => row.count)
+          data: null //boroughData.map((row) => row.count)
         }
       ]
     }
@@ -45,12 +39,10 @@ export default {
       const data = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
       const dataJSON = await data.json() // results would bypass promise if i were using a ref
       console.log(dataJSON)
+      //create dict here
+      // this.chartData.labels == dict
+      //this.cchartData.datasets.data = blah blah
       this.loaded = true
-      /* dataJSON.forEach((entry) => {
-        if (entry.arrest_boro == 'K') {
-          // change borougdata borough count
-        }
-      }) */
       /*
       let dataLocation = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.geojson')
       let dataLocationJSON = dataLocation.json()
