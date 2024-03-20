7594cc246ed9f52de5deaf412f84075f9ee3cca5
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2024-03-20 13:15:13 -0400

    map works -- leaflet puts markers in antarctica

--- coolproject/src/components/MapChart.vue
+++ coolproject/src/components/MapChart.vue
@@ -1,13 +1,17 @@
 <template>
-  <div style="height: 600px; width: 800px">
+  <div v-if="geoLoaded" style="height: 600px; width: 800px">
     <l-map ref="map" :zoom="zoom" :center="[47.41322, -1.219482]">
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
-      <l-marker :lat-lng="markerLatLng"></l-marker>
-      <!-- vfor here -->
+      <l-marker
+        v-for="item in markers"
+        :key="item.id"
+        :lat-lng="item.position"
+        @click="alert(item)"
+      />
       <l-geo-json :geojson="GeoJSON" :options-style="geoStyler"></l-geo-json>
     </l-map>
   </div>
@@ -29,8 +33,15 @@ export default {
     return {
       zoom: 2,
       GeoJSON: undefined,
-      geoLoaded: undefined,
-      markerLatLng: [47.41322, -1.219482]
+      geoLoaded: false,
+      markers: [
+        {
+          id: 'test',
+          position: { lat: 51.505, lng: -0.09 },
+          draggable: false,
+          visible: true
+        }
+      ]
     }
   },
   async mounted() {
@@ -38,6 +49,26 @@ export default {
     const data = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
     const dataJSON = await data.json()
     this.GeoJSON = dataJSON
+    dataJSON.forEach((element) => {
+      if (element.arrest_boro == 'S') {
+        this.markers.push({
+          id: 'm',
+          position: {
+            lat: Number(element.geocoded_column.coordinates[0].toFixed(3)),
+            lng: Number(element.geocoded_column.coordinates[1].toFixed(3))
+          },
+          draggable: false,
+          visible: true
+        })
+      }
+    })
+    console.log(this.markers)
+    this.geoLoaded = true
+  },
+  methods: {
+    alert(item) {
+      alert('this is ' + JSON.stringify(item))
+    }
   }
 }
 </script>
