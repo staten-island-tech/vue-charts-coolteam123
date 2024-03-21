8e01a966b2ee7b4d177ae4e0b678195531fda920
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2024-03-21 13:14:10 -0400

    geoJSON implemented, but json requires mapping

--- coolproject/src/components/MapChart.vue
+++ coolproject/src/components/MapChart.vue
@@ -1,17 +1,11 @@
 <template>
   <div v-if="geoLoaded" style="height: 600px; width: 800px">
-    <l-map ref="map" :zoom="zoom" :center="[47.41322, -1.219482]">
+    <l-map ref="map" :zoom="12" :center="[40.571944, -74.146944]">
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
-      <l-marker
-        v-for="item in markers"
-        :key="item.id"
-        :lat-lng="item.position"
-        @click="alert(item)"
-      />
       <l-geo-json :geojson="GeoJSON" :options-style="geoStyler"></l-geo-json>
     </l-map>
   </div>
@@ -19,55 +13,39 @@
 
 <script>
 import 'leaflet/dist/leaflet.css'
+import { ref } from 'Vue'
 import L from 'leaflet'
-import { LMap, LTileLayer, LGeoJson, LMarker } from '@vue-leaflet/vue-leaflet'
+import { LMap, LTileLayer, LGeoJson } from '@vue-leaflet/vue-leaflet'
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LGeoJson,
-    LMarker
+    LGeoJson
   },
   data() {
     return {
-      zoom: 2,
       GeoJSON: undefined,
       geoLoaded: false,
-      markers: [
-        {
-          id: 'test',
-          position: { lat: 51.505, lng: -0.09 },
-          draggable: false,
-          visible: true
-        }
-      ]
+      geoStyler: (feature) => ({
+        opacity: feature.properties.code / 100000
+      })
     }
   },
   async mounted() {
     this.geoLoaded = false
-    const data = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
+    const data = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.geojson')
     const dataJSON = await data.json()
     this.GeoJSON = dataJSON
-    dataJSON.forEach((element) => {
-      if (element.arrest_boro == 'S') {
-        this.markers.push({
-          id: 'm',
-          position: {
-            lat: Number(element.geocoded_column.coordinates[0].toFixed(3)),
-            lng: Number(element.geocoded_column.coordinates[1].toFixed(3))
-          },
-          draggable: false,
-          visible: true
-        })
-      }
-    })
-    console.log(this.markers)
+    const newDataJSON = ref({})
+    dataJSON.features.forEach((element) => (newDataJSON.value.geometry = element.geometry))
+    console.log(newDataJSON)
+    // map newDataJSON using https://leafletjs.com/examples/geojson/ format
     this.geoLoaded = true
   },
   methods: {
     alert(item) {
-      alert('this is ' + JSON.stringify(item))
+      alert(`${item.desc} \n${item.date}`)
     }
   }
 }
