5bbbb545ef17859eb4b2772b8ff8481ff2dd4f8c
evY000 <000evan001@gmail.com>
2024-03-25 12:54:27 -0400

    stuff2

--- coolproject/src/App.vue
+++ coolproject/src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-/* import { Bar} from 'vue.chartjs' */
 import HelloWorld from './components/HelloWorld.vue'
 import { ref, onBeforeMount } from 'vue' // ref allows for editability, onMounted grabs api when component added on screen(for constant updates)
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+import BarChart from 'coolproject/src/components/chart.vue'
+
+export default {
+  name: 'App',
+  components: { BarChart }
+}
 const crimeData = ref('')
 const crimeLocationData = ref('')
 async function getAPI() {
@@ -15,25 +18,23 @@ async function getAPI() {
   crimeData.value = dataJSON
   let dataLocation = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.geojson')
   let dataLocationJSON = dataLocation.json()
-  crimeLocationData.value = dataLocationJSON // for leaflet
+  crimeLocationData.value = dataLocationJSON
+   // for leaflet
   // https://play.vuejs.org/#eNp9UtuK2zAQ/ZVBL+tAYkPaUhqcQLfsQwu90Pat6oNQxrE2tiSksZNg/O8d2bvpli5rMLbOBZ0z0iDee5/3HYqNKKMOxhNEpM7vpDWtd4FggIDVEpy9xcoF/Ow6SzBCFVwLN+y8kRZAWu1sJPAuUoRtsmS/fi+klfYfY6bixWrIFrDdwZCckyPvVdMh+9RJGYIKSdeZFDWRj5ui8MH0inD1br3ev32zCqhDZ6hFS4S6tkarhjBSrrxR4bJqnT7m2rWF7iK5Fs8YtIkYCykWOdVos6BOKQB/8vvobLZ4wNMiEembX31cY+S3LOYB8Wh4Qdj6hkPxqtybHvoVl9xKkfqAsXMvKWBzxMsDnJu9FKwHfsp6vRuGScUwjGNZMPI/Z1WLT9my4M3SnsU1gFgKijz+yhymNnyU02Sl4Bl402D46snw8UixmWeeONU07vRpwih0uHzEdY36+Ax+H88Jk+JbwIihRymuHKlwQJrpux9f8Mz/V7J1+65h9Qvkd4yu6VLGWXbb2T3HfqKb0n6cLqSxh5/x7kxo42OpFDQpx0kvBd/KDy9U/xv3Vf568vEBi/EP1K8G5A==
 }
 onBeforeMount(() => {
   getAPI()
 })
+
 </script>
 <template>
   <header>
-    <img alt="Vue logo" class="logo bg-black w-24 h-24" src="@/assets/logo.svg" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+   <BarChart></BarChart>
       <!--       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav> -->
-    </div>
+    
   </header>
 
   <!-- <RouterView /> -->
---
+++ coolproject/src/components/chart.vue
@@ -0,0 +1,20 @@
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+export default{
+    name : 'Chart',
+    components : {Bar},
+    data(){
+        return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script>
