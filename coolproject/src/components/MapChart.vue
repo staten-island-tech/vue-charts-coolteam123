e012dc1987ddc9c685d8be2daae0b2f4e7fa0f37
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2024-03-26 13:08:42 -0400

    map restricted to 100 api calls -- consider other vue3 repo

--- coolproject/src/components/MapChart.vue
+++ coolproject/src/components/MapChart.vue
@@ -6,8 +6,11 @@
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
-      <l-marker v-for="item in markers" :key="item.id" :lat-lng="item.position">
-        <l-popup> {{ item.id }}, {{ item.date }}, {{ item.boro }}</l-popup>
+      <l-marker v-for="item in markers" :key="item.name" :lat-lng="item.position">
+        <l-popup>
+          {{ item.name }} <br />
+          {{ item.date }}<br />{{ item.boro }}</l-popup
+        >
       </l-marker>
       <!-- vfor here -->
       <l-geo-json :geojson="GeoJSON" :options-style="geoStyler"></l-geo-json>
@@ -18,6 +21,7 @@
 <script>
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
+import { ref } from 'vue'
 import { LMap, LTileLayer, LGeoJson, LMarker, LPopup } from '@vue-leaflet/vue-leaflet'
 
 export default {
@@ -35,44 +39,59 @@ export default {
       geoStyler: (feature) => ({
         opacity: feature.properties.code / 100000
       }),
-      markers: [
+      markers: ref([
         {
-          id: 'test',
+          name: 'test',
+          date: 'test',
+          boro: 'test',
           position: { lat: 51.505, lng: -0.09 },
           draggable: false,
-          visible: true
+          visible: Boolean(false)
         }
-      ]
+      ])
     }
   },
   async mounted() {
     this.geoLoaded = false
     const data = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
     const dataJSON = await data.json()
-    dataJSON.forEach((element) => {
-      if (element.arrest_boro == 'S') {
-        this.markers.push({
-          id: `DESCRIPTION: ${element.pd_desc}`,
-          date: `DATE: ${element.arrest_date.slice(0, 10)}`,
-          boro: `BORO:${element.arrest_boro}`,
-          position: {
-            lat: Number(element.geocoded_column.coordinates[1].toFixed(3)),
-            lng: Number(element.geocoded_column.coordinates[0].toFixed(3))
-          },
-          draggable: false,
-          visible: true
-        })
+    dataJSON.slice(0, 100).forEach((element) => {
+      this.markers.push({
+        name: `DESCRIPTION: ${element.pd_desc}`,
+        date: `DATE: ${element.arrest_date.slice(0, 10)}`,
+        boro: `BORO:${element.arrest_boro}`,
+        position: {
+          lat: Number(element.geocoded_column.coordinates[1].toFixed(3)),
+          lng: Number(element.geocoded_column.coordinates[0].toFixed(3))
+        },
+        draggable: false,
+        visible: true,
+        opacity: 0.5
+      })
+    })
+    this.markers.forEach((marker) => {
+      console.log(marker.boro)
+      if (marker.boro === 'BORO:K') {
+        marker.visible === true
+        console.log(marker.visible)
       }
     })
-    console.log(dataJSON)
     this.geoLoaded = true
   },
   methods: {
     alert(item) {
       alert(`${item.id} \n${item.date}`)
     }
+    /*     boroughChange() {
+      this.markers.forEach((marker) => {
+        if (marker.boro == 'K') {
+          marker.visible == true
+        } 
+      }) 
+    }*/
   }
 }
 </script>
 
 <style></style>
+<!-- consider switching to vue3-google-map w/ marker cluster-->
