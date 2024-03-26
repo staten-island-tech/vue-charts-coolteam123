890365cbecc949634ab402d35366993d4dc6b0e5
RichardM1337 <richardm246@nycstudents.net>
2024-03-25 20:38:59 -0400

    Update MapChart.vue

--- coolproject/src/components/MapChart.vue
+++ coolproject/src/components/MapChart.vue
@@ -6,6 +6,10 @@
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
+      <l-marker v-for="item in markers" :key="item.id" :lat-lng="item.position">
+        <l-popup> {{ item.id }}, {{ item.date }}, {{ item.boro }}</l-popup>
+      </l-marker>
+      <!-- vfor here -->
       <l-geo-json :geojson="GeoJSON" :options-style="geoStyler"></l-geo-json>
     </l-map>
   </div>
@@ -13,15 +17,16 @@
 
 <script>
 import 'leaflet/dist/leaflet.css'
-import { ref } from 'Vue'
 import L from 'leaflet'
-import { LMap, LTileLayer, LGeoJson } from '@vue-leaflet/vue-leaflet'
+import { LMap, LTileLayer, LGeoJson, LMarker, LPopup } from '@vue-leaflet/vue-leaflet'
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LGeoJson
+    LGeoJson,
+    LMarker,
+    LPopup
   },
   data() {
     return {
@@ -29,23 +34,42 @@ export default {
       geoLoaded: false,
       geoStyler: (feature) => ({
         opacity: feature.properties.code / 100000
-      })
+      }),
+      markers: [
+        {
+          id: 'test',
+          position: { lat: 51.505, lng: -0.09 },
+          draggable: false,
+          visible: true
+        }
+      ]
     }
   },
   async mounted() {
     this.geoLoaded = false
-    const data = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.geojson')
+    const data = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
     const dataJSON = await data.json()
-    this.GeoJSON = dataJSON
-    const newDataJSON = ref({})
-    dataJSON.features.forEach((element) => (newDataJSON.value.geometry = element.geometry))
-    console.log(newDataJSON)
-    // map newDataJSON using https://leafletjs.com/examples/geojson/ format
+    dataJSON.forEach((element) => {
+      if (element.arrest_boro == 'S') {
+        this.markers.push({
+          id: `DESCRIPTION: ${element.pd_desc}`,
+          date: `DATE: ${element.arrest_date.slice(0, 10)}`,
+          boro: `BORO:${element.arrest_boro}`,
+          position: {
+            lat: Number(element.geocoded_column.coordinates[1].toFixed(3)),
+            lng: Number(element.geocoded_column.coordinates[0].toFixed(3))
+          },
+          draggable: false,
+          visible: true
+        })
+      }
+    })
+    console.log(dataJSON)
     this.geoLoaded = true
   },
   methods: {
     alert(item) {
-      alert(`${item.desc} \n${item.date}`)
+      alert(`${item.id} \n${item.date}`)
     }
   }
 }
