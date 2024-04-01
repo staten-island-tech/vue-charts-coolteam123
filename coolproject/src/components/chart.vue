29f450d687d661fa9a4744ffe59b69a215970c9b
evY000 <000evan001@gmail.com>
2024-03-31 23:49:03 -0400

    stuff7

--- coolproject/src/components/chart.vue
+++ coolproject/src/components/chart.vue
@@ -4,40 +4,38 @@ import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, Li
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 const apistuff = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json');
     const stuffjson = await apistuff.json();
+
+
 export default{
     name : 'BarChart',
     components : {Bar},
+
     data: () => ({
+  
       loaded : false,
-      chartData : null,
-    }), 
+      chartData : { 
+        labels : ['test']
+              
+      }}), 
     async mounted(){
       this.loaded = false
     
-    try {
-   //   stuff.data.filter((card)=> { if (card.name.toUpperCase() === Upper.valueOf()){ 
-    // const FirstName = card.films
-   
+    try {   
    stuffjson.forEach((thing) =>  {
     const gender = thing.perp_sex
-    console.log(gender)
-    const { userlist } =  gender 
+  /*   console.log(gender) */
+    const userlist  =  gender 
       this.chartdata = userlist
       this.loaded = true
-   })
-    
-      
-      
+      console.log(userlist)
+   })   
     } catch (error) {
       console.error(error)
   }
     }}
 </script>
-<template>
   <template>
   <div class="container">
     <Bar v-if="loaded" :data="chartData" />
   </div>
-</template>
-
 </template>
