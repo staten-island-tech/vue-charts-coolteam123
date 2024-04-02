1214c30d3b9d05f165c6ff85a348505203627658
evY000 <000evan001@gmail.com>
2024-04-02 12:32:56 -0400

    stuff12

--- coolproject/src/router/index.js
+++ coolproject/src/router/index.js
@@ -4,8 +4,7 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
+    { path: '/',
       name: 'home',
       component: HomeView
     },
